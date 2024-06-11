a0ea1c200b5cf382e085de4566134345309382a1
1024xx4 <teraleaf1024.4@gmail.com>
2024-06-11 19:53:20 +0900

    Feat: Read を実装する

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,7 +1,21 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.engine import Result, Row
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
+
+
+def get_task_with_done(db: Session) -> list[tuple[int, str, bool]]:
+    result: Result = db.execute(  # イテレータとして定義
+        select(
+            task_model.Task.id,
+            task_model.Task.title,
+            task_model.Done.id.isnot(None).label("done"),  # Done.id が存在するときは done=True
+        ).outerjoin(task_model.Done)
+    )
+
+    return result.all()  # すべての DB records を取得
 
 
 def create_task(db: Session, task_create: task_schema.TaskCreate) -> task_model.Task:
